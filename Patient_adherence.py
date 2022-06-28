0810f56e95aa75de7b22f4746ad304a5dad8f604
tanushsunil <107385446+tanushsunil@users.noreply.github.com>
2022-06-28 22:58:16 +0530

    Update Patient_adherence.py

--- Patient_adherence.py
+++ Patient_adherence.py
@@ -11,7 +11,7 @@ st.title("Patient Adherence")
 
 Selected = option_menu(
     menu_title=None,
-    options=["Home", "Models Tester", "PA Finder", "About us"],
+    options=["Home", "PA Finder", "About us"],
     icons=["house-fill", "speedometer2", "hexagon-half", "people-fill"],
     default_index=0,
     orientation="horizontal"
@@ -33,13 +33,10 @@ if Selected == "PA Finder":
 elif Selected == "Home":
     st.header("What are we solving?")
     st.text("The problem addressed by this model is finding out if the patient is medically\nadherent or not. clinical studies have demonstrated that only 50-70% of patients\nadhere properly to prescribed drug therapy. This behavior of adherence failure\ncan cause health issues, hospitalization risk and even death. Patient Adherence\ninsights can prove useful for \n\n     1.Doctors who prescribe drugs\n     2.Drug / Medication producers\n     3.Government")
-    image = Image.open("web_app\Images\Home_embed_image.png")
-    st.image(image)
     st.title("What is our model focused on?")
     st.text("This model is focused on the prediction of adherence behavior with individual\nselection. The dataset utilized is historically captured through a medication\nevent monitoring system. When the group who are prone to be non-adherent is\naccurately identified and targeted, it makes the way for improving patient care\nand helps Healthcare workers to assess and develop new strategies.")
 
-elif Selected == "Models Tester":
-    Model_tester.main()
+
 
 
 
