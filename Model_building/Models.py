9f0d47b4e7e966aaa48cb92368c54df786989fe7
tanushsunil <107385446+tanushsunil@users.noreply.github.com>
2022-07-10 12:37:46 +0530

    Update Models.py

--- Model_building/Models.py
+++ Model_building/Models.py
@@ -1,6 +1,7 @@
-# Packages--------------------------------------------------------------------------->Importing all the required packages
+# Packages---------------------------->Importing all the required packages
 import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
@@ -10,29 +11,26 @@ from sklearn.metrics import confusion_matrix
 from sklearn import metrics
 import pickle
 
-# Importing Data------------------------------->>Reading .CSV & doing a light processing
-dataset = pd.read_csv(
-    r'C:\Users\tanus\Desktop\patient_adherence\Dataset\patient_data_processed.csv')
+# Importing Data------------>>Reading .CSV & doing a light data processing
+dataset = pd.read_csv(r'C:\Users\tanus\Desktop\patient_adherence\Dataset\patient_data_processed.csv')
 
 le = LabelEncoder()
 dataset['Adherence_n'] = le.fit_transform(dataset['Adherence'])
 dataset = dataset.drop(['Unnamed: 0', 'Adherence'], axis='columns')
 
-# Feature Selection--------------------------------------------------->>Selecting the required features to feed the model
+# Feature Selection---->>Selecting the required features to feed the model
 x = dataset.iloc[:, [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 14, 15, 16, 17]]
 y = dataset.iloc[:, 9]
 
-print(dataset.iloc[:, 12:14])
-
-# Data splitting------------------------------------------------------>>Splitting data into training and testing datasets
+# Data splitting------>>Splitting data into training and testing datasets
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=0)
 
 
-# Logistic Regression CLassification------------------------------------------------------------------>>Type 1 Classifier
+# Logistic Regression CLassification----------------->>Type 1 Classifier
 def lr_prediction():    
     global lr
-    lr = LogisticRegression()
+    lr = LogisticRegression(C=0.5)
     lr.fit(x_train, y_train)
 
     y_pred = lr.predict(x_test)
@@ -44,11 +42,11 @@ def lr_prediction():
     print('The confusion matrix for this model is \n', cm)
 
 
-# K-Nearest Neighbor Classifier---------------------------------------------------------------------->>Type 2 Classifier
+# K-Nearest Neighbor Classifier---------------------->>Type 2 Classifier
     
 def knn_prediction():   
     global knn 
-    knn = KNeighborsClassifier(n_neighbors=2)
+    knn = KNeighborsClassifier(n_neighbors=5)
     knn.fit(x_train, y_train)
 
     y_pred = knn.predict(x_test)
@@ -60,7 +58,7 @@ def knn_prediction():
     print('The confusion matrix for this model is \n', cm)
 
 
-# Decision Tree Classifier--------------------------------------------------------------------------->>Type 3 Classifier
+# Decision Tree Classifier---------------------------->>Type 3 Classifier
 def deci_prediction():
     global deci_tree
     deci_tree = DecisionTreeClassifier(criterion='entropy', random_state=0)
@@ -74,10 +72,12 @@ def deci_prediction():
     cm = confusion_matrix(y_test, y_pred)
     print('The confusion matrix for this model is \n', cm)
 
-# Random Forest Classifier--------------------------------------------------------------------------->>Type 4 Classifier
+    
+
+# Random Forest Classifier---------------------------->>Type 4 Classifier
 def random_forest():
     global rf_pred
-    rf_pred = RandomForestClassifier(n_estimators=10, criterion = 'entropy', random_state= 0)
+    rf_pred = RandomForestClassifier(n_estimators=2, criterion = 'entropy', random_state= 0)
     rf_pred.fit(x_train, y_train)
 
     y_pred = rf_pred.predict(x_test)
@@ -90,10 +90,6 @@ def random_forest():
     print("Confusion Matrix")
     print(cm)
 
-    results = rf_pred.predict_proba(x_test)[0]
-
-    print(results)
-
 # Predicting results
 lr_prediction()
 knn_prediction()
@@ -101,18 +97,21 @@ deci_prediction()
 random_forest()
 
 
-# Pickling Process---------------------------------------------------------------->>Exporting the Models into .sav files
-filename = ('K-Nearest_Neighbor.sav')
+# Pickling Process----------------->>Exporting the Models into .sav files
+"""filename = ('K-Nearest_Neighbor.sav')
 pickle.dump(knn, open(filename,'wb'))
 
 filename_2 = ('Logistic_Regression.sav')
 pickle.dump(lr, open(filename_2,'wb'))
 
 filename_3 = ('Decision_Tree_Classifier.sav')
-pickle.dump(deci_tree, open(filename_3,'wb'))
+pickle.dump(deci_tree, open(filename_3,'wb'))"""
+
+filename_4 = ('Random_Forest_Classifier.sav')
+pickle.dump(rf_pred, open(filename_4,'wb'))
 
 
-# Motivation :)--------------------------------------------------------------------------------------->>The way to go!!
+# Motivation :)----------------------------------------->>The way to go!!
 print("\n\nThat's the spirit! Only deployment is pending.\nHope it's a piece of cake for you!")
 
 # What are you looking for? Thats the end!
